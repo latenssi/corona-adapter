a97292064741a800bf4dc01570d07009fd313775
Lauri Junkkari <latenssi@gmail.com>
2020-03-26 14:49:25 +0200

    Update

--- server.js
+++ server.js
@@ -1,29 +1,75 @@
 const KoaCors = require("@koa/cors");
-const KoaRouter = require("koa-router")();
-const axios = require("axios");
-const PassThrough = require("stream").PassThrough;
+const router = require("koa-router")();
 const server = new (require("koa"))();
-// const cache = new (require("streaming-cache"))();
-// const { AsyncParser } = require("json2csv");
 
-KoaRouter.get("/json", ctx => {
-  const stream = PassThrough();
+const Chain = require("stream-chain");
+const { parser: streamJsonParser } = require("stream-json");
+const { pick: streamJsonPicker } = require("stream-json/filters/Pick");
+const { stringer: streamJsonStringer } = require("stream-json/Stringer");
+const { Transform: CSVTransform } = require("json2csv");
 
-  ctx.type = "json";
-  ctx.body = stream;
+const axios = require("axios");
+const fs = require("fs");
 
+function attachDataStream(stream) {
+  // fs.createReadStream("./temp.json").pipe(stream);
   axios({
     method: "get",
     url:
       "https://w3qa5ydb4l.execute-api.eu-west-1.amazonaws.com/prod/finnishCoronaData",
     responseType: "stream"
   }).then(response => response.data.pipe(stream));
+}
+
+function streamData(ctx, path, type) {
+  const pipeline = [
+    streamJsonParser(), // Parse json byte stream
+    streamJsonPicker({ filter: path }), // Pick the parts we want
+    streamJsonStringer() // Convert back to byte stream
+  ];
+
+  if (type === "csv") {
+    pipeline.push(
+      new CSVTransform({
+        fields: [
+          "id",
+          "date",
+          "healthCareDistrict",
+          "infectionSourceCountry",
+          "infectionSource"
+        ]
+      })
+    );
+  }
+
+  const chain = new Chain(pipeline);
+
+  attachDataStream(chain.input);
+
+  // WTF? removing these causes the stream to never end..
+  chain.on("data", () => {});
+  chain.on("end", () => {});
+
+  ctx.type = "json";
+  ctx.body = chain.output;
+}
+
+router.get("/:path.:type", ctx => {
+  const { path, type } = ctx.params;
+  if (
+    ["confirmed", "recovered", "deaths"].indexOf(path) > -1 &&
+    ["json", "csv"].indexOf(type) > -1
+  ) {
+    streamData(ctx, path, type);
+    return;
+  }
+  ctx.status = 404;
 });
 
 server
   .use(KoaCors())
-  .use(KoaRouter.routes())
-  .use(KoaRouter.allowedMethods());
+  .use(router.routes())
+  .use(router.allowedMethods());
 
 if (!module.parent) {
   server.listen(process.env.PORT || 8000);
